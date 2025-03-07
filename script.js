93ce4e9cc15c88374f87619612e780ea2c8a96f9
D1ORdev <oktamjonovmuslimbek@gmail.com>
2025-03-07 17:13:53 +0500

    arrow

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const HelloWorld=function()
+{
+    console.log("Hello world!");
+}
+HelloWorld();
+const CalculateXojiaka=function(a,b)
+{
+    return a+b;
+}
+console.log(CalculateXojiaka(9,7));
+
+// function expression
+
+
+// arrow function =>
+
+const CalculateXojiaka1=(a,b)=>
+{
+    return a+b;
+}
+
+// amal 1ta bolsa => bir qatorda blok sxemasiz yoziladi
+console.log(CalculateXojiaka1(9,8));
