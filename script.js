470b0e606633c38745dff4cf68c59df2601e79f9
D1ORdev <oktamjonovmuslimbek@gmail.com>
2025-03-11 21:36:32 +0500

    homework done

--- script.js
+++ script.js
@@ -1,24 +1,231 @@
 
-const HelloWorld=function()
-{
-    console.log("Hello world!");
-}
-HelloWorld();
-const CalculateXojiaka=function(a,b)
-{
-    return a+b;
-}
-console.log(CalculateXojiaka(9,7));
+// const HelloWorld=function()
+// {
+//     console.log("Hello world!");
+// }
+// HelloWorld();
+// const CalculateXojiaka=function(a,b)
+// {
+//     return a+b;
+// }
+// console.log(CalculateXojiaka(9,7));
 
-// function expression
+// // function expression
 
 
-// arrow function =>
+// // arrow function =>
 
-const CalculateXojiaka1=(a,b)=>
-{
-    return a+b;
-}
+// const CalculateXojiaka1=(a,b)=>
+// {
+//     return a+b;
+// }
 
-// amal 1ta bolsa => bir qatorda blok sxemasiz yoziladi
-console.log(CalculateXojiaka1(9,8));
+// // amal 1ta bolsa => bir qatorda blok sxemasiz yoziladi
+// console.log(CalculateXojiaka1(9,8));
+
+//let a=4;
+//let b=5;
+//let c=a+b;
+//console.log(c);
+
+// funksiya -> kodni qayta qayta yozmaslik
+// js funksiya , declaration,expression,arrow,
+// FUNCTION DECLERATION
+// 3 TA ASOSIY QISM  key->function nomi->
+
+//function  SHowHelloworld()
+//{
+    //console.log("Hello world!");
+//}
+//SHowHelloworld();
+
+//let age=17;
+//console.log(age);
+//function sayHellotoUser(name)
+// {
+//     let age=20;//
+//     console.log(`Hello ${name},he is ${age} years old`);
+// }
+
+// sayHellotoUser("Sardor");
+// console.log(age);
+
+// () -> argument dynamic malumotlar
+
+// ""  ` ` -> backstick/
+
+// argumentdi ichida name
+
+// lakal va global malumot turi
+
+// lokal malumot turi -> functiondi  blok sxemasi ichida boladi
+
+// global malumot turi -> functiondi blok sxemasi ichida bolmasa
+
+//let age=> 17 edi , funksiya ichida 20 bersak ham u tasir qilmadi ,funksiya ichida 20 tashqarida esa 17
+
+//const ae=18+(getCalculations(4,7));
+//console.log(ae);
+
+//function getCalculations(a,b)
+//{
+ //   return a+b;  // malumotdi qaytaradi ozida saqlab turadi, i ustida amal bajarsa boladi 
+   // ozidan keyin ishlatmidi 
+ //   let tyu=25;
+  //  console.log(tyu);
+//}
+
+
+// function decleration => biz uni script ichida xohlagan joyimizda chaqiramiz
+
+// const HelloWorld=function()
+// {
+//     console.log("Hello world!");
+// }
+// HelloWorld();
+// const CalculateXojiaka=function(a,b)
+// {
+//     return a+b;
+// }
+// console.log(CalculateXojiaka(9,7));
+
+// // function expression => function nomi yoq boladi
+// // function expression
+
+
+// // arrow function =>
+
+// const CalculateXojiaka1=(a,b)=>
+// {
+//     return a+b;
+// }
+
+// // amal 1ta bolsa => bir qatorda blok sxemasiz yoziladi
+// console.log(CalculateXojiaka1(9,8));
+
+
+// metod -> tayyor funksiya => obyektda,arrayda,stringda =>
+
+//     const hojiaka='Helloworld!'; // 12 ta belgi bor
+//     console.log(hojiaka.length);
+   
+//     // lenght -> metod -> property
+//     // qiymat 
+//     // metod  funksiya  () =>argument
+//     console.log('Bu harf',hojiaka.charAt(2));
+//     console.log(hojiaka[5]);  // property
+//     console.log(hojiaka[5]='A');
+//     console.log('Harflarni kattalashtrdik',hojiaka.toUpperCase());
+//    console.log('harflarni kichraytirdik',hojiaka.toLowerCase());
+//   console.log('kesish:',hojiaka.slice(0,-5));// -minus son 
+//   // => metod faqat return qiymat qaytaradi
+//    console.log('toza kesish',hojiaka.substring(0,-5));
+  
+//    // slice=> substring
+//   console.log(hojiaka);
+
+//let a=4;
+//let b=5;
+//let c=a+b;
+//console.log(c);
+
+// funksiya -> kodni qayta qayta yozmaslik
+// js funksiya , declaration,expression,arrow,
+// FUNCTION DECLERATION
+// 3 TA ASOSIY QISM  key->function nomi->
+
+//function  SHowHelloworld()
+//{
+    //console.log("Hello world!");
+//}
+//SHowHelloworld();
+
+//let age=17;
+//console.log(age);
+//function sayHellotoUser(name)
+// {
+//     let age=20;//
+//     console.log(`Hello ${name},he is ${age} years old`);
+// }
+
+// sayHellotoUser("Sardor");
+// console.log(age);
+
+// () -> argument dynamic malumotlar
+
+// ""  ` ` -> backstick/
+
+// argumentdi ichida name
+
+// lakal va global malumot turi
+
+// lokal malumot turi -> functiondi  blok sxemasi ichida boladi
+
+// global malumot turi -> functiondi blok sxemasi ichida bolmasa
+
+//let age=> 17 edi , funksiya ichida 20 bersak ham u tasir qilmadi ,funksiya ichida 20 tashqarida esa 17
+
+//const ae=18+(getCalculations(4,7));
+//console.log(ae);
+
+//function getCalculations(a,b)
+//{
+ //   return a+b;  // malumotdi qaytaradi ozida saqlab turadi, i ustida amal bajarsa boladi 
+   // ozidan keyin ishlatmidi 
+ //   let tyu=25;
+  //  console.log(tyu);
+//}
+
+
+// function decleration => biz uni script ichida xohlagan joyimizda chaqiramiz
+
+// const HelloWorld=function()
+// {
+//     console.log("Hello world!");
+// }
+// HelloWorld();
+// const CalculateXojiaka=function(a,b)
+// {
+//     return a+b;
+// }
+// console.log(CalculateXojiaka(9,7));
+
+// // function expression => function nomi yoq boladi
+// // function expression
+
+
+// // arrow function =>
+
+// const CalculateXojiaka1=(a,b)=>
+// {
+//     return a+b;
+// }
+
+// // amal 1ta bolsa => bir qatorda blok sxemasiz yoziladi
+// console.log(CalculateXojiaka1(9,8));
+
+
+// metod -> tayyor funksiya => obyektda,arrayda,stringda =>
+
+     const hojiaka='Xujaabad';
+     console.log('kesish:',hojiaka.slice(1,5));
+     
+     
+     
+     // 12 ta belgi bor
+     //console.log(hojiaka.length);
+   
+    // lenght -> metod -> property
+    // qiymat 
+    // metod  funksiya  () =>argument
+    //console.log('Kichraytirildi!',);
+     //console.log(hojiaka[6]);  // property
+    // console.log(hojiaka[5]='J');
+     //console.log('Harflarni kattalashtrdik',hojiaka.toUpperCase());
+//    console.log('harflarni kichraytirdik',hojiaka.toLowerCase());
+//   console.log('kesish:',hojiaka.slice(0,-5));// -minus son 
+//   // => metod faqat return qiymat qaytaradi
+//    console.log('toza kesish',hojiaka.substring(0,-5));
+  
+//    // slice=> substring
+//   console.log(hojiaka);
